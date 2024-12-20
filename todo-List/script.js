54b27c21b61eb13bcbb10cc74eee354728b3a9cb
MD MAHABUBUR RAHMAN <rahmanmdmahabubur666@gmail.com>
2024-12-20 19:29:28 +0600

    Add files via upload

---
+++ todo-List/script.js
@@ -0,0 +1,49 @@
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const form = document.getElementById('todo-form');
+    const input = document.getElementById('todo-input');
+    const todoLIst = document.getElementById('todo-list');
+   
+   
+    form.addEventListener('submit', (e) =>{
+       e.preventDefault();
+
+       const newTask = input.value.trim();
+
+       if(newTask){
+         
+        const listItem = document.createElement('li');
+           listItem.classList.add('todo-item');
+           listItem.innerHTML=`<span>${newTask}</span>
+           <button class='complete-btn'>Complete</button>
+          <button class='delete-btn'>delete</button>
+           ` ;
+           
+           todoLIst.appendChild(listItem);
+           input.value = '';
+
+         
+
+           listItem.querySelector('.complete-btn').addEventListener('click', ()=>{
+              
+            listItem.classList.toggle('completed');
+
+
+           });
+
+        listItem.querySelector('.delete-btn').addEventListener('click', ()=>{
+
+         listItem.remove();
+
+        })
+
+
+       }
+
+       
+
+         
+   
+    });
+   
+   });
